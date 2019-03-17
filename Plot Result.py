f63928cda4b5cd395c3c4a888319a78cfeb0105e
helminster32 <helmin.david@gmail.com>
2019-03-17 22:39:29 +0100

    Prueba

---
+++ Plot Result.py
@@ -0,0 +1,15 @@
+import re
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import cv2
+
+x = cv2.imread(".\\DataBase\\06100.jpg")
+cv2.circle(x,(615,289),3,(0,0,255), -1)
+cv2.circle(x,(654,289),3,(0,0,255), -1)
+cv2.circle(x,(640,245),3,(0,0,245), -1)
+cv2.circle(x,(629,245),3,(0,0,255), -1)
+cv2.imshow("Window", x)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
